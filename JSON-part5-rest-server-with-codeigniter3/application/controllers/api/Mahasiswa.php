5e9ff5745b168852fec88c52d4f369a6e611b1c1
aLvinpri <alvhient@gmail.com>
2019-10-08 22:22:50 +0700

    vid 8 done

--- JSON-part5-rest-server-with-codeigniter3/application/controllers/api/Mahasiswa.php
+++ JSON-part5-rest-server-with-codeigniter3/application/controllers/api/Mahasiswa.php
@@ -27,9 +27,7 @@ class Mahasiswa extends CI_Controller
 
 		// Configure limits on our controller methods
 		// Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-		$this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
-		$this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
-		$this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+		$this->methods['index_get']['limit'] = 10;
 	}
 
 	public function index_get()
